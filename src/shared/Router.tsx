d1589bed050a2ea42cbc60199615abf45e972c3e
jominuk <jominuk1025@naver.com>
2023-06-12 20:51:15 +0900

    :sparkles: git logo button

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "src/ui/Layout";
+import Header from "src/ui/Header";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Layout>
+        <Header />
         <Routes></Routes>
       </Layout>
     </BrowserRouter>
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <StContainer>
+      <h2>나만의 TodoList 😋</h2>
+      <StButton>
+        <StImg alt="git" src={require("../image/git.jpg")}></StImg>
+      </StButton>
+    </StContainer>
+  );
 };
-
 export default Header;
+
+const StContainer = styled.div`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px 0px 20px;
+  border-radius: 10px;
+  border-bottom: 3px solid white;
+  color: white;
+`;
+
+const StButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
+
+const StImg = styled.img``;
