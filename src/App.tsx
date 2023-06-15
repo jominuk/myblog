c345907e91482291ba203ecddbcd48191af1f6b5
jominuk <jominuk1025@naver.com>
2023-06-15 21:22:56 +0900

    🐛background 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "./App.css";
 import Router from "./shared/Router";
+import styled from "styled-components";
 
 const App = () => {
   return (
     <>
-      <div className="App">
+      <StBack>
         <Router />
-      </div>
+      </StBack>
     </>
   );
 };
 
 export default App;
+
+const StBack = styled.div`
+  background-image: url("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbipmmj%2FbtsjInaKc2R%2FdgPRwgWrhISK731kYRcBN0%2Fimg.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
